1c88c918a257fffd32f94a39e2f0e5d6cbd8fd40
Topology12 <duong120503mk@gmail.com>
2024-07-05 21:26:14 +0700

    Buoi_1

---
+++ buoi1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int num3 = 0;
+int main()
+{
+    // Cú pháp khai báo biến trong c++
+    // <Kiểu dữ liệu> tên biến or <Kiểu dữ liệu> tên biến = <giá trị gán>
+    int ageQuynh;
+    ageQuynh = 19;
+    int ageAdou = 21;
+    cout<< "Be tho nho hon Adou " << ageAdou-ageQuynh<<" hehe" << endl; 
+    return 0;
+}
+//Bài tập là
+// Khai báo các loại biến int , float, double, char
+// Tạo 1 mảng với loại dữ liệu bất kì
+// Viết 1 hàm 
+// push code lên git
+ 
